c19b0a569c13e3cbbd02a1a68458784c1ab00f28
Tamim Sherif <101951441+Tamim21@users.noreply.github.com>
2025-04-19 15:42:49 +0200

    git commit -m "Stop tracking .env file"

--- Server/src/main/java/com/example/demo/DemoApplication.java
+++ Server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        //EnvLoader.load();
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
+        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+
+
         SpringApplication.run(DemoApplication.class, args);
     }
 }
